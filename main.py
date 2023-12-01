2b4b573cf4c8391acdc40131b896c48058900c1d
agnanp <primahardika@gmail.com>
2023-12-02 02:35:07 +0700

    add model selector and gpu toggle

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import streamlit as st
 import time 
+import os
 
 from langchain.llms import Ollama
 
-st.set_page_config(page_title="Mistral Chatbot")
 
-llm = Ollama(
-    model="mistral", num_gpu=1
-)
+st.set_page_config(page_title="💬 Ollama Chatbot")
 
 with st.sidebar:
-    st.title('💬 Mistral Chatbot')
+    st.title('💬 Ollama Chatbot')
+    
+    selected_model = st.selectbox('Choose a model', ['Mistral', 'Llama2', 'Code Llama'], key='selected_model')
 
     system_prompt = st.text_area(
             label="System Prompt",
             value="You are a helpful assistant who answers questions in short sentences."
             )
-    
+    gpu_on = st.toggle('Activate GPU')
+# Select the model 
+if selected_model == "Mistral":
+    llm_model = "mistral"
+elif selected_model == "Llama2":
+    llm_model = "llama2"
+else:
+    llm_model = "codellama"
+
+# Activate the GPU
+if gpu_on:
+    activate_gpu = 1
+else:
+    activate_gpu = 0
+
+llm = Ollama(
+    model=llm_model, num_gpu=activate_gpu
+)   
+
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
@@ -35,14 +53,23 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-def generate_llama2_response(prompt_input):
+def generate_llm_response(prompt_input):
     string_dialogue = f"{system_prompt}. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = llm.__call__(prompt=f"{string_dialogue} {prompt_input} Assistant: ") 
+    try:
+        output = llm.__call__(prompt=f"{string_dialogue} {prompt_input} Assistant: ") 
+    except Exception:
+        with st.status("Downloading data..."):
+            st.write("pulling manifest")
+            os.system(f"ollama pull {llm_model}")
+            st.write("download complete!")
+            time.sleep(0.1)
+            st.rerun()
+
     return output
 
 # User-provided prompt
@@ -55,7 +82,7 @@ if prompt := st.chat_input():
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant", avatar="💬"):
         with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
+            response = generate_llm_response(prompt)
             placeholder = st.empty()
             full_response = ''
         for item in response:
