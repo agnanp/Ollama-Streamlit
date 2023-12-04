437b7d58990fa8ef80967b4f3202bb312db70ae3
agnanp <primahardika@gmail.com>
2023-12-04 15:58:25 +0700

    add model descriptions

--- main.py
+++ main.py
@@ -13,8 +13,33 @@ st.set_page_config(page_title="Ollama Chatbot", page_icon="💬")
 with st.sidebar:
     st.title('💬 Ollama Chatbot')
     
+    st.divider()
     # Select the model
     selected_model = st.selectbox('Choose a model', ['Mistral', 'Llama2', 'Code Llama'], key='selected_model')
+    
+    if selected_model == "Mistral":
+        llm_model = "mistral"
+        st.caption("""
+                   The Mistral 7B model released by Mistral AI. 
+                   Mistral 7B model is an Apache licensed 7.3B parameter model. 
+                   It is available in both instruct (instruction following) and text completion.
+                   """) 
+    elif selected_model == "Llama2":
+        llm_model = "llama2"
+        st.caption("""
+                   Llama 2 is released by Meta Platforms, Inc. 
+                   This model is trained on 2 trillion tokens, and by default supports a context length of 4096. 
+                   Llama 2 Chat models are fine-tuned on over 1 million human annotations, and are made for chat.
+                   """) 
+    else:
+        llm_model = "codellama"
+        st.caption("""
+                   Code Llama is a model for generating and discussing code, built on top of Llama 2. 
+                   It’s designed to make workflows faster and efficient for developers and make it easier for people to learn how to code. 
+                   It can generate both code and natural language about code. 
+                   Code Llama supports many of the most popular programming languages used today, including Python, C++, Java, PHP, Typescript (Javascript), C#, Bash and more.
+                   """) 
+    st.divider()
 
     # Edit system prompt 
     system_prompt = st.text_area(
@@ -22,16 +47,9 @@ with st.sidebar:
             value="You are a helpful assistant who answers questions in short sentences."
             )
     
+    st.divider()
     # Toggle to activate GPU 
     gpu_on = st.toggle('Activate GPU')
-
-# Select the model 
-if selected_model == "Mistral":
-    llm_model = "mistral"
-elif selected_model == "Llama2":
-    llm_model = "llama2"
-else:
-    llm_model = "codellama"
 
 # Activate the GPU
 if gpu_on:
@@ -57,7 +75,7 @@ llm_chain = LLMChain(llm=Ollama(model=llm_model, num_gpu=activate_gpu), prompt=p
 
 # Display current messages from StreamlitChatMessageHistory
 for msg in msgs.messages:
-    if msg.type == "Human":
+    if msg.type == "human":
         with st.chat_message(msg.type, avatar="🧑‍💻"):
             st.write(msg.content)
     else:
@@ -67,7 +85,7 @@ for msg in msgs.messages:
 # Clear chat messages
 def clear_chat_history():
     msgs.clear()
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history, use_container_width=True)
 
 # User input new prompt, generate, and display a new response
 if prompt := st.chat_input():
